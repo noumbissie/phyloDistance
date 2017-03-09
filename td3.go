e00c150a512a86df67f4dc509809e26c3f20555f
gaelle <gaelle.noumbissie@gmail.com>
2017-03-09 16:58:12 +0100

    un autre commit

--- td3.go
+++ td3.go
@@ -36,7 +36,7 @@ var tabfilename = []string{
 	"NC_002662.fna",
 }
 
-
+  var mat_sym [15][15]float64
 
 
 func CompressString(data_s string)string{
@@ -53,6 +53,8 @@ func CompressString(data_s string)string{
 
 
 func ProcessFile(s string)string{
+	
+	//fmt.Println("Debut")
 	file, _ := os.Open(s)
 
 	reader := bufio.NewReader(file)
@@ -64,11 +66,12 @@ func ProcessFile(s string)string{
 	next_line_is_dna := false
 	for scanner.Scan() {
 		lineNumber++
-
-		if shortTreatment && (lineNumber > 40000) {
+         
+		if shortTreatment && (lineNumber > 4000) {
 			break
 		}
 		//var val_str string
+		fmt.Println("mileu",lineNumber)
 		val_str := scanner.Text()
 		if len(val_str) > 0 {
 			if val_str[0] == '>' {
@@ -106,58 +109,100 @@ func getsize(s string)int64{
 }
 
 
-func distance(a string,b string )float32{
-		
-	za_b:=getsize(CompressString(a+b))
-	za:=getsize(CompressString(a))
-	zb:=getsize(CompressString(b))
-	za_a:=getsize(CompressString(a+a))
-	zb_b:=getsize(CompressString(b+b))
+func distance(a string,b string )float64{
 	
-	var d float32
-	d=(float32)((za_b/(za+zb))-(za_a/(4*za))-(zb_b/(4*zb)))
+	//fmt.Println("calul !!")	
+	za_b:=(float64)(getsize(CompressString(a+b)))
+	//fmt.Println("calcul 1")
+	za:=(float64)(getsize(CompressString(a)))
+	//fmt.Println("calcul 2")
+	zb:=(float64)(getsize(CompressString(b)))
+	//fmt.Println("calcul 3")
+	za_a:=(float64)(getsize(CompressString(a+a)))
+	zb_b:=(float64)(getsize(CompressString(b+b)))
 	
-	fmt.Println(d)
+	var d float64=(float64)((za_b/(za+zb))-(za_a/(4*za))-(zb_b/(4*zb)))
+	
+	//~ fmt.Println(d)
+		//~ fmt.Println(za)
+		//~ fmt.Println(zb)
+		//~ fmt.Println(za_b)
+		//~ fmt.Println(za_a)
+		//~ fmt.Println(zb_b)
+	//~ 
 	
 	return d
 }
 
+
+
+
+
 func matrix_sym(){
 	
-var(i,j int32)	
-
-
+   for i := 0; i < 15; i++ {
+	   fmt.Println("out ! ")
+        for j := i+1; j < 15; j++ {
+			fmt.Println("in !!!")
+		    mat_sym[i][j]=distance(ProcessFile(tabfilename[i]),ProcessFile(tabfilename[j]))	
+		    
+		    fmt.Println(mat_sym[i][j])
+		    fmt.Println("in 2!!!")
+	    }
+	}
 	
 	
+	for i := 1; i < 15; i++ {
+        for j := 0; j < i; j++ {
+			mat_sym[i][j]=mat_sym[j][i]
+		}
+	}
+	
+	fmt.Println(" Matrix not empty !! ")
+	
+	file, err := os.Create("matrix.dat")
+    if err != nil {
+       fmt.Println(err)
+       return
+    }
+
+	
+	for i := 0; i < 15; i++ {
+        for j := 0; j < 15; j++ {
+	        
+	       fmt.Fprintf(file, "%2f ",mat_sym[i][j])
+	        
+	    }
+	    fmt.Println("")
+    }
 }
+
 
 
 
 func main() {
 	
-	file1 := `NC_000964.fna`
-	file2 := `NC_002662.fna`
+	//file1 := `NC_000964.fna`
+	//file2 := `NC_000913.fna`
     
     
-	_, err := os.Stat(file1)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "the file %s doesn't exist! \n", file1)
-		os.Exit(1)
-	}
-	
-	
-	
-	
 	fmt.Println(" ...in progress ! ");
 	
-	str1:=ProcessFile(file1)
-	str2:=ProcessFile(file2)
+	matrix_sym()
+    //ioutil.WriteFile("test.data",tobytes(mat_sym) , 0644)
 	
-	val:=distance(str1,str2)
+	fmt.Println(" Matrix ready !! ")
 	
-	//fileComp:=CompressString(str)
 	
-	fmt.Println(val)
+	//var str1 string=ProcessFile(file1)
+	//var str2 string=ProcessFile(file2)
+	
+	//var val float64=distance(str1,str2)
+	
+	////fileComp:=CompressString(str)
+	
+	//fmt.Println(val)
+	
     // Done.
-    fmt.Println("File compressed !")
+   fmt.Println("End  !")
 }
